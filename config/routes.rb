7c8ab2a3e10145698d1a15a805774879f16ebb52
rombod <rambodmagid@yahoo.com>
2017-05-17 18:09:18 -0600

    123

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  
+  
+  get 'contact', to: 'static_pages#contact'
+
+  get 'projects', to: 'static_pages#projects'
+
+  get 'about', to: 'static_pages#about'
+
+  get 'index', to: 'static_pages#index'
+
+  root :to => 'static_pages#index' 
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
